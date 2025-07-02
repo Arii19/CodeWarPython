9105c8db45ecddeec14f2866003cefb190b8406e
Arii19 <arianer19@hotmail.com>
2025-07-02 20:40:35 -0300

    aperfeiçoando a api

---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from datetime import datetime
+from database import Base
+
+class Livro(Base):
+    __tablename__ = "livros"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, nullable=False)
+    descricao = Column(String, nullable=False)
+    genero = Column(String, nullable=False)
+    data_inclusao = Column(DateTime, default=datetime.utcnow)
+    data_edicao = Column(DateTime, nullable=True)
+    data_exclusao = Column(DateTime, nullable=True)
---
+++ schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class LivroBase(BaseModel):
+    nome: str
+    descricao: str
+    genero: str
+
+class LivroCreate(LivroBase):
+    pass
+
+class LivroUpdate(LivroBase):
+    pass
+
+class LivroResponse(LivroBase):
+    id: int
+    data_inclusao: datetime
+    data_edicao: Optional[datetime]
+    data_exclusao: Optional[datetime]
+
+    class Config:
+        orm_mode = True
