af4aa7e31c9dea5517de99a0f20a97ecd23d75a3
Arii19 <arianer19@hotmail.com>
2025-07-07 20:58:12 -0300

    mudando para postgre

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./livros.db"
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("PASSWORD")
+host = os.getenv("Hostname")
+port = os.getenv("PORT", "5432")
+db = os.getenv("Database")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
