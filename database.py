60603b27102873ae04600890a0e48b81ab70164b
Arii19 <arianer19@hotmail.com>
2025-07-07 21:42:30 -0300

    adaptando para o postgre .env

--- database.py
+++ database.py
@@ -1,20 +1,9 @@
-import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from dotenv import load_dotenv
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Carrega o conteúdo do arquivo .env
-load_dotenv()
-
-# Pegue a URL corretamente
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
-
-if not SQLALCHEMY_DATABASE_URL:
-    raise ValueError("❌ Variável EXTERNAL_DATABASE_URL não encontrada no .env!")
+# Conexão direta (sem usar variável de ambiente)
+SQLALCHEMY_DATABASE_URL = "postgresql://ari:caIN95fnEcr4S7Mk1gkyamxXA4oMwPHj@dpg-d1m5pt63jp1c73ebv31g-a.oregon-postgres.render.com/livros_533j"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()t 
+Base = declarative_base()
