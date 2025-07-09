0738ac7ca1b94a01f228ec7febacad372cf80690
Arii19 <arianer19@hotmail.com>
2025-07-09 20:29:24 -0300

    voltando para o sqllite

--- database.py
+++ database.py
@@ -3,14 +3,6 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-# Conexão direta (sem usar variável de ambiente)
-SQLALCHEMY_DATABASE_URL = "postgresql://ari:caIN95fnEcr4S7Mk1gkyamxXA4oMwPHj@dpg-d1m5pt63jp1c73ebv31g-a.oregon-postgres.render.com/livros_533j"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
-
-
 
 DATABASE_URL = "sqlite:///./livros.db"
 
