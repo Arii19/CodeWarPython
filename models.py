981b74e40231fe884a32c0b39a2d3dec22a9d258
Arii19 <arianer19@hotmail.com>
2025-07-09 20:21:07 -0300

    subindo testes unitáros com testclient e add gitignore

--- models.py
+++ models.py
@@ -1,20 +1,16 @@
-from sqlalchemy import Column, DateTime, Integer, String, DateTime
-from datetime import datetime
+from sqlalchemy import Column, DateTime, Integer, String
+from datetime import datetime, timezone
 from database import Base
-
-# Define o modelo Livro que representa a tabela 'livros' no banco de dados
-
-# Nome da tabela no banco
 
 class Livro(Base):
     __tablename__ = "livros"
 
-# Colunas da tabela
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String(255), nullable=False)
     autor = Column(String(255), nullable=False)
     descricao = Column(String(255), nullable=False)
     genero = Column(String(255), nullable=False)
-    data_inclusao = Column(DateTime, default=datetime.utcnow)
-    data_edicao = Column(DateTime, nullable=True)
-    data_exclusao = Column(DateTime, nullable=True)
+    
+    data_inclusao = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    data_edicao = Column(DateTime(timezone=True), nullable=True)
+    data_exclusao = Column(DateTime(timezone=True), nullable=True)
