ce522c8dfe456663e63043b45863866964af7d8e
Arii19 <arianer19@hotmail.com>
2025-07-13 22:14:45 -0300

    inserindo livros na api

--- graficos.py
+++ graficos.py
@@ -4,4 +4,11 @@ import streamlit as st
 
 BASE_URL = "https://api-biblioteca-lg6i.onrender.com/livros"
 
-st.title("Biblioteca API - Gráficos")
+def buscar_livros_por_autor(autor):
+    try:
+        response = requests.get(f"{BASE_URL}/autor/{autor}")
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        st.error(f"Erro ao buscar livros: {e}")
+        return []
